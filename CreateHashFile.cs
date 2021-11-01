7ad5fe5e6e16fc9e71ca860beb22c70924d96455
WarlockCyberTutorials <49326569+WarlockCyberTutorials@users.noreply.github.com>
2021-11-01 15:12:21 -0300

    Adicionando arquivos ao projeto

---
+++ CreateHashFile.cs
@@ -0,0 +1,46 @@
+﻿using CreateHashFile.Enum;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class CreateHashFile
+    {
+        public static string ObterHashDoArquivo(TipoDeHash.Hash pTipoHash, string pCaminhoDoArquivo)
+        {
+            var stringBuilder = new StringBuilder();
+
+            using (var file = new FileStream(pCaminhoDoArquivo, FileMode.Open))
+            {
+                HashAlgorithm hash;
+
+                if (pTipoHash == TipoDeHash.Hash.Md5)
+                {
+                    hash = new MD5CryptoServiceProvider();
+                }
+                else
+                {
+                    hash = new SHA1CryptoServiceProvider();
+                }
+
+                if (hash != null)
+                {
+                    var retVal = hash.ComputeHash(file);
+                    for (int i = 0; i < retVal.Length; i++)
+                    {
+                        stringBuilder.Append(retVal[i].ToString("x2"));
+                    }
+                }
+
+                hash.Dispose();
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ Enum/TipoDeHash.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class TipoDeHash
+    {
+        public enum Hash
+        {
+            Md5 = 1,
+            SHA1 = 2
+        }
+    }
+}
